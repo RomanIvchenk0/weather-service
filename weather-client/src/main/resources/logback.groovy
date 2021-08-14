86a435466fa46321e88a38ffac3014868bdcbbfc
Roman I <roman.ivchenko@rakuten.com>
2021-08-14 22:40:26 +0300

    Change groovy to xml logging config to skip adding groovy in classpath

--- weather-client/src/main/resources/logback.groovy
+++
@@ -1,9 +0,0 @@
-appender("STDOUT", ConsoleAppender) {
-    encoder(PatternLayoutEncoder) {
-        pattern = "%d{dd-HH:mm:ss.SSS}:%-5level[%thread]: %logger{36} - %msg%n"
-    }
-}
-
-logger("com.foo", DEBUG, ["CONSOLE"])
-
-root(INFO, ["STDOUT"])
